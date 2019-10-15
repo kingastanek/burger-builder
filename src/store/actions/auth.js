c85c500cc767f2ab2972c83223739bc66b70524c
kingastanek <kin.stanek@gmail.com>
2019-10-15 21:54:51 +0200

    Handling Auto-Sign-In with a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -24,9 +22,6 @@ export const authFail = error => {
 };
 
 export const logout = () => {
-  // localStorage.removeItem('token');
-  // localStorage.removeItem('expirationDate');
-  // localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
   };
@@ -41,7 +36,7 @@ export const logoutSucceed = () => {
 export const checkAuthTimeout = expirationTime => {
   return {
     type: actionTypes.AUTH_CHECK_TIMEOUT,
-    expirationTime: expirationTime
+    expirationTime
   };
 };
 
@@ -62,23 +57,7 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
-  return dispatch => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      dispatch(logout());
-    } else {
-      const expirationDate = new Date(localStorage.getItem("expirationDate"));
-      if (expirationDate <= new Date()) {
-        dispatch(logout());
-      } else {
-        const userId = localStorage.getItem("userId");
-        dispatch(authSuccess(token, userId));
-        dispatch(
-          checkAuthTimeout(
-            (expirationDate.getTime() - new Date().getTime()) / 1000
-          )
-        );
-      }
-    }
+  return {
+    type: actionTypes.AUTH_CHECK_STATE
   };
 };
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -7,7 +7,7 @@ export function* logoutSaga(action) {
   yield localStorage.removeItem("token");
   yield localStorage.removeItem("expirationDate");
   yield localStorage.removeItem("userId");
-  yield put(actions.logoutSucceed);
+  yield put(actions.logoutSucceed());
 }
 
 export function* checkAuthTimeoutSaga(action) {
@@ -42,5 +42,25 @@ export function* authUserSaga(action) {
     yield put(actions.checkAuthTimeout(response.data.expiresIn));
   } catch (error) {
     yield put(actions.authFail(error.response.data.error));
+  }
+}
+
+export function* authCheckStateSaga(action) {
+  const token = yield localStorage.getItem("token");
+  if (!token) {
+    yield put(actions.logout());
+  } else {
+    const expirationDate = yield new Date(localStorage.getItem("expirationDate"));
+    if (expirationDate <= new Date()) {
+      yield put(actions.logout());
+    } else {
+      const userId = yield localStorage.getItem("userId");
+      yield put(actions.authSuccess(token, userId));
+      yield put(
+        actions.checkAuthTimeout(
+          (expirationDate.getTime() - new Date().getTime()) / 1000
+        )
+      );
+    }
   }
 }
